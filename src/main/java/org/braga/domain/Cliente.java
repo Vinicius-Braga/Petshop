6b67de9fc12843dd3b5e7c9c4074eaf3ab96176c
unknown <vinyvick3@gmail.com>
2024-04-18 10:57:48 -0300

    Atualizando a estruturta do projeto

---
+++ src/main/java/org/braga/domain/Cliente.java
@@ -0,0 +1,32 @@
+package org.braga.domain;
+
+public class Cliente {
+
+    private Integer code;
+    private String name;
+    private String cpf;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
---
+++ src/main/java/org/braga/domain/Pet.java
@@ -0,0 +1,34 @@
+package org.braga.domain;
+
+public class Pet {
+
+    private Integer code;
+
+    private String name;
+
+    private String race;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+}
