015d875d59a185719ad35495b9ad22be76af8be9
unknown <vinyvick3@gmail.com>
2024-04-18 11:28:50 -0300

    Created DAO functionality for client

--- src/main/java/org/braga/dao/IClienteDAO.java
+++ src/main/java/org/braga/dao/IClienteDAO.java
@@ -1,4 +1,15 @@
 package org.braga.dao;
 
+import org.braga.domain.Cliente;
+
 public interface IClienteDAO {
+
+    Integer register (Cliente cliente);
+
+    Integer consult (Cliente cliente);
+
+    Integer update (Cliente cliente);
+
+    Integer delete (Cliente cliente);
+    ;
 }
